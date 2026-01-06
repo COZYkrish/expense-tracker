b1db7fc2f4c0518255af096929e9865af256a025
COZYkrish <cozykrish2916@gmail.com>
2026-01-07 01:48:42 +0530

    added admin dashboard and made id for admin

--- routes/user.py
+++ routes/user.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect
 from flask_login import login_required, current_user
 from models.loan import Loan
 from extensions import db
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
 
+
 @user_bp.route("/")
 @login_required
 def dashboard():
     loans = Loan.query.filter_by(user_id=current_user.id).all()
     return render_template("user/dashboard.html", loans=loans)
+
 
 @user_bp.route("/apply", methods=["POST"])
 @login_required
@@ -18,11 +20,13 @@ def apply_loan():
         user_id=current_user.id,
         borrower=current_user.username,
         amount=request.form["amount"],
-        tenure=request.form["tenure"]
+        tenure=request.form["tenure"],
+        status="Pending"
     )
     db.session.add(loan)
     db.session.commit()
     return redirect("/user")
+
 
 @user_bp.route("/loans")
 @login_required
@@ -34,5 +38,6 @@ def user_loans_api():
             "amount": loan.amount,
             "tenure": loan.tenure,
             "status": loan.status
-        } for loan in loans
+        }
+        for loan in loans
     ])
