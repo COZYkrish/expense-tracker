5ce183c6577f2fcc950942ecdec4d5057f4fca23
COZYkrish <cozykrish2916@gmail.com>
2026-01-07 22:32:28 +0530

    added toggle button

---
+++ static/js/theme.js
@@ -0,0 +1,17 @@
+const toggle = document.getElementById("themeToggle");
+const root = document.documentElement;
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    root.setAttribute("data-theme", savedTheme);
+}
+
+// Toggle theme
+toggle.addEventListener("click", () => {
+    const isDark = root.getAttribute("data-theme") === "dark";
+    const newTheme = isDark ? "light" : "dark";
+
+    root.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+});
